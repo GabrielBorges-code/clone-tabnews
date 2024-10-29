71e1763b693bdd00315d420549c7c771e008bf0e
Gabriel B Moura <gabriel.moura2009@hotmail.com>
2024-10-29 12:18:45 -0300

    chore: reorganize

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,12 @@
-import Image from "next/image";
+import WarleyCareca from "./warley-careca";
 
 function Home() {
   return (
     <>
       <h1>Olá mundo!</h1>
-      <p>O Warley é um jovem careca!</p>
-
-      <Image 
-        src="/static/images/warley-careca.webp"
-        alt="Warley careca"
-        width={500}
-        height={500}
-      />
+      <WarleyCareca />
     </>
-  )
+  );
 }
 
 export default Home;

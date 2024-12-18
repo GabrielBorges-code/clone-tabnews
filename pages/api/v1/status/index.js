161fce0e0638d11fa42273b442c81ed1da88804c
Gabriel B Moura <gabriel.moura2009@hotmail.com>
2024-12-18 16:57:27 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,17 +3,45 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  // console.time("TempoDasOperações");
-  const [maxConnections, version, openedConnections] = await database.query(
-    "SHOW max_connections; SHOW server_version; SELECT count(*) AS opened_connections FROM pg_stat_activity;",
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
   );
-  // console.timeEnd("TempoDasOperações");
+  const databaseMaxConnectionsValue =
+    await databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  // minha implantação das consultas
+
+  // const [maxConnections, version, openedConnections] = await database.query(
+  //   "SHOW max_connections; SHOW server_version; SELECT count(*) AS opened_connections FROM pg_stat_activity;",
+  // );
 
   response.status(200).json({
     updated_at: updatedAt,
-    max_connections: Number(maxConnections.rows[0].max_connections),
-    version: version.rows[0].server_version,
-    opened_connections: Number(openedConnections.rows[0].opened_connections),
+    dependencies: {
+      database: {
+        version: databseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+
+    // meus retornos da api
+    // max_connections: Number(maxConnections.rows[0].max_connections),
+    // version: version.rows[0].server_version,
+    // opened_connections: Number(openedConnections.rows[0].opened_connections),
   });
 }
 
